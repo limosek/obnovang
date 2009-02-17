4723312fa3d04c589571299e8286b89150a8296e
limo <limo@04f62543-cabe-4c31-b458-38295e5ff4ef>
2009-02-17 15:57:19 +0000

    Pripravy na totaly novou obnovu

---
+++ obnovang/oldobnova.mk
@@ -0,0 +1,6 @@
+# Makefile include for old obnova system
+# Will be deprecated
+
+# Obnova old files
+COPYFILES += src/old/obnova:/etc/init.d/obnova src/old/zaloha:/etc/init.d/zaloha src/old/main:/etc/init.d/main src/old/obnova-included.conf:/etc/obnova-included.conf src/old/rcS:/bin/obnovang-stage2 src/old/common-functions:/etc/common-functions src/old/obnovang:/sbin/init src/old/obnovang:/bin/obnovang src/old/cpipe:/bin/cpipe src/old/upipe:/bin/upipe src/old/udpcast:/bin/udpcast
+
