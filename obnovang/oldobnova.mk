634dd50fa05b9b845b576267e887051dc1eaae0f
limo <limo@04f62543-cabe-4c31-b458-38295e5ff4ef>
2012-04-13 07:37:04 +0000

    Repairs

--- obnovang/oldobnova.mk
+++ obnovang/oldobnova.mk
@@ -2,6 +2,6 @@
 # Will be deprecated
 
 # Obnova old files
-COPYFILES += src/old/obnova:/etc/init.d/obnova src/old/zaloha:/etc/init.d/zaloha src/old/main:/etc/init.d/main src/old/obnova-included.conf:/etc/obnova-included.conf src/old/rcS:/bin/obnovang-stage2 src/old/common-functions:/etc/common-functions src/old/obnovang:/sbin/init src/old/obnovang:/bin/obnovang src/old/cpipe:/bin/cpipe src/old/upipe:/bin/upipe src/old/udpcast:/bin/udpcast
+COPYFILES += src/old/obnova:/etc/init.d/obnova src/old/zaloha:/etc/init.d/zaloha src/old/main:/etc/init.d/main src/old/obnova-included.conf:/etc/obnova-included.conf src/old/rcS:/bin/obnovang-stage2 src/old/common-functions:/etc/common-functions src/old/obnovang:/sbin/init src/old/obnovang:/bin/obnovang src/old/cpipe:/bin/cpipe src/old/upipe:/bin/upipe src/old/udpcast:/bin/udpcast src/old/timer:/bin/timer
 
 COPYFILES += etc/obnova-embed.conf:/etc/obnova-embed.conf
